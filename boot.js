ec97540a328f840b2a72b39bc5924b8eeb9bd26e
Domenico-lel <domenicolella2@gmail.com>
2023-12-02 16:08:04 +0100

    aggiunti i nomi-ruol dei dipendenti di una azienda

---
+++ boot.js
@@ -0,0 +1,42 @@
+/*tilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sotto forma di stringhe*/
+
+
+// creo un array con le iformazioni di ogni membro del team
+const teamUsers = [
+    {
+        nome: "Waine Bernet",
+        ruolo: "Founder e CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela caroll",
+        ruolo: "chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Meadia Menager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scot Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Grafic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    }
+];
